aae539d1d6dda13259af6f5ef9a2e21d0fca7628
Fulya Akan <fulyaakan@outlook.com>
2026-06-22 23:41:09 +0300

    Initial commit with .gitignore

---
+++ News/News/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  News
+//
+//  Created by fulya akan on 22.06.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
