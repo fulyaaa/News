208e301a8640551394fb5cba03a237ecacb16b21
Fulya Akan <fulyaakan@outlook.com>
2026-06-29 17:53:08 +0300

    Add Network Layer and Article model

---
+++ News/News/Utils/Article.swift
@@ -0,0 +1,29 @@
+//
+//  Article.swift
+//  News
+//
+//  Created by fulya akan on 24.06.2026.
+//
+import Foundation
+
+struct ArticleResponse: Codable, Sendable {
+    let status: String
+    let totalResults: Int
+    let articles: [Article]
+}
+
+struct Article: Codable, Sendable {
+    let source: Source
+    let author: String?
+    let title: String
+    let description: String
+    let url: String
+    let urlToImage: String?
+    let publishedAt: String
+    let content: String?
+}
+
+struct Source: Codable, Sendable {
+    let id: String?
+    let name: String
+}
---
+++ News/News/Utils/NetworkManager.swift
@@ -0,0 +1,48 @@
+//
+//  NetworkManager.swift
+//  News
+//
+//  Created by fulya akan on 24.06.2026.
+//
+import Foundation
+
+protocol NetworkManagerProtocol {
+    func getTopHeadlines(completion: @escaping (Result<[Article], Error>) -> Void)
+    func searchNews(query: String, completion: @escaping (Result<[Article], Error>) -> Void)
+}
+
+class NetworkManager: NetworkManagerProtocol {
+    
+    static let shared = NetworkManager()
+    
+    private let apiKey = "daff04363a184a4488bd59e2b6ce9fd0"
+    private let baseURL = "https://newsapi.org/v2/"
+    
+    func getTopHeadlines(completion: @escaping (Result<[Article], Error>) -> Void) {
+        let urlString = ("\(baseURL)top-headlines?country=us&apiKey=\(apiKey)")
+        performRequest(urlString: urlString, completion: completion)
+    }
+    
+    func searchNews(query: String, completion: @escaping (Result<[Article], Error>) -> Void) {
+        let urlString = "\(baseURL)/everything?q=\(query)&apiKey=\(apiKey)"
+        performRequest(urlString: urlString, completion: completion)
+    }
+    
+    private func performRequest(urlString: String, completion: @escaping (Result<[Article], Error>) -> Void) {
+        guard let url = URL(string: urlString) else { return }
+        URLSession.shared.dataTask(with: url) { (data, response, error) in
+            if let error = error {
+                completion(.failure(error))
+                return
+            }
+            guard let data = data else { return }
+            
+            do {
+                let result = try JSONDecoder().decode(ArticleResponse.self, from: data)
+                completion(.success(result.articles))
+            } catch {
+                completion(.failure(error))
+            }
+        }.resume()
+    }
+}
